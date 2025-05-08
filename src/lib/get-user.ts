8f37b952ddd861f4031b9fe7cd603a22f1a52bbe
Candi Agusta <mugiwaraagusta@gmail.com>
2025-05-08 10:52:06 +0700

    add: indra, krisna, fara

--- src/lib/get-user.ts
+++ src/lib/get-user.ts
@@ -1,31 +1,43 @@
 interface User {
-	name: string;
-	image: string;
+  name: string;
+  image: string;
 }
 
 const users: Record<string, User> = {
-	"laksjdlkasjdasdwe": {
-		name: "Aknez Nuril Mahmuda",
-		image: "/user/laksjdlkasjdasdwe.jpeg"
-	},
-	"qpwoeqwpoksajdasdas": {
-		name: "Andini Putri Ramadhani",
-		image: "/user/qpwoeqwpoksajdasdas.png"
-	},
-	"nnnaosudqoiwjdpqoj": {
-		name: "Firdiyatus Sholihah",
-		image: "/user/nnnaosudqoiwjdpqoj.jpeg"
-	},
-	"yiwuyerewiuryweri": {
-		name: "Elisa Febrian Sholeha",
-		image: "/user/yiwuyerewiuryweri.jpeg"
-	}
+  "laksjdlkasjdasdwe": {
+    name: "Aknez Nuril Mahmuda",
+    image: "/user/laksjdlkasjdasdwe.jpeg"
+  },
+  "qpwoeqwpoksajdasdas": {
+    name: "Andini Putri Ramadhani",
+    image: "/user/qpwoeqwpoksajdasdas.png"
+  },
+  "nnnaosudqoiwjdpqoj": {
+    name: "Firdiyatus Sholihah",
+    image: "/user/nnnaosudqoiwjdpqoj.jpeg"
+  },
+  "yiwuyerewiuryweri": {
+    name: "Elisa Febrian Sholeha",
+    image: "/user/yiwuyerewiuryweri.jpeg"
+  },
+  "uwyegfkjsdhfklsdjfl": {
+    name: "Indra Prasetyo Wijaya",
+    image: "/user/uwyegfkjsdhfklsdjfl.jpeg"
+  },
+  "zzmndwlyaosknjdaskld": {
+    name: "Fara Derliana",
+    image: "/user/zzmndwlyaosknjdaskld.jpeg"
+  },
+  "poijfwjhkajjdalksjdas": {
+    name: "Krisna Choiril Andika",
+    image: "/user/poijfwjhkajjdalksjdas.jpeg"
+  }
 }
 
 export function getUser(queryParam: string | null): User | null {
-	if (!queryParam) {
-		return null;
-	}
+  if (!queryParam) {
+    return null;
+  }
 
-	return users[queryParam];
+  return users[queryParam];
 }
