d9eb374f8f25c21325cd785268d487a57f860a07
Candi Agusta <mugiwaraagusta@gmail.com>
2025-05-23 09:00:31 +0700

    add: alip, fathan, sohib, putri

--- src/lib/get-user.ts
+++ src/lib/get-user.ts
@@ -31,6 +31,22 @@ const users: Record<string, User> = {
   "poijfwjhkajjdalksjdas": {
     name: "Krisna Choiril Andika",
     image: "/user/poijfwjhkajjdalksjdas.jpeg"
+  },
+  "usudfkjsdnflwefottywefi": {
+    name: "Hasan Sohibul Anwar",
+    image: "/user/usudfkjsdnflwefottywefi.jpeg"
+  },
+  "amfnsdjfheyierfpwefk": {
+    name: "Alif Triadi Agung Wicaksono",
+    image: "/user/amfnsdjfheyierfpwefk.jpeg"
+  },
+  "frrelwlkjelkjflaskdfjalksjd": {
+    name: "Fathan Maulana Rifqi Hartono",
+    image: "/user/frrelwlkjelkjflaskdfjalksjd.jpeg"
+  },
+  "eqpowqpowdnmzdnajhsdaskdj": {
+    name: "Eka Putri Yuliana",
+    image: "/user/eqpowqpowdnmzdnajhsdaskdj.jpeg"
   }
 }
 
