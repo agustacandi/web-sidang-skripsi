ba68e69570513954d69d346d577b041b1ee9be9b
Candi Agusta <mugiwaraagusta@gmail.com>
2026-07-06 17:14:51 +0700

    Update user records in get-user.ts
    Removed multiple user entries and added a new user.

--- src/lib/get-user.ts
+++ src/lib/get-user.ts
@@ -4,53 +4,9 @@ interface User {
 }
 
 const users: Record<string, User> = {
-  "laksjdlkasjdasdwe": {
-    name: "Aknez Nuril Mahmuda",
-    image: "/user/laksjdlkasjdasdwe.jpeg"
-  },
-  "qpwoeqwpoksajdasdas": {
-    name: "Andini Putri Ramadhani",
-    image: "/user/qpwoeqwpoksajdasdas.png"
-  },
-  "nnnaosudqoiwjdpqoj": {
-    name: "Firdiyatus Sholihah",
-    image: "/user/nnnaosudqoiwjdpqoj.jpeg"
-  },
-  "yiwuyerewiuryweri": {
-    name: "Elisa Febrian Sholeha",
-    image: "/user/yiwuyerewiuryweri.jpeg"
-  },
-  "uwyegfkjsdhfklsdjfl": {
-    name: "Indra Prasetyo Wijaya",
-    image: "/user/uwyegfkjsdhfklsdjfl.jpeg"
-  },
-  "zzmndwlyaosknjdaskld": {
-    name: "Fara Derliana",
-    image: "/user/zzmndwlyaosknjdaskld.jpeg"
-  },
-  "poijfwjhkajjdalksjdas": {
-    name: "Krisna Choiril Andika",
-    image: "/user/poijfwjhkajjdalksjdas.jpeg"
-  },
-  "usudfkjsdnflwefottywefi": {
-    name: "Hasan Sohibul Anwar",
-    image: "/user/usudfkjsdnflwefottywefi.jpeg"
-  },
-  "amfnsdjfheyierfpwefk": {
-    name: "Alif Triadi Agung Wicaksono",
-    image: "/user/amfnsdjfheyierfpwefk.jpeg"
-  },
-  "frrelwlkjelkjflaskdfjalksjd": {
-    name: "Fathan Maulana Rifqi Hartono",
-    image: "/user/frrelwlkjelkjflaskdfjalksjd.jpeg"
-  },
-  "eqpowqpowdnmzdnajhsdaskdj": {
-    name: "Eka Putri Yuliana",
-    image: "/user/eqpowqpowdnmzdnajhsdaskdj.jpeg"
-  },
-  "uewyroqmasnasdasdkmsj": {
-    name: "Angga Raksa Prabaswara Putra",
-    image: "/user/uewyroqmasnasdasdkmsj.png"
+  "alksdjlkasjdlkajskldjaksljd": {
+    name: "Riski Rosalinda",
+    image: "/user/alksdjlkasjdlkajskldjaksljd.jpeg"
   }
 }
 
